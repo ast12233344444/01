06724ddec54bbb37540b632feb42f13774243ab2
ast12233344444 <97659984+ast12233344444@users.noreply.github.com>
2022-01-20 13:20:30 +0300

    data str. completed

---
+++ dev/api.js
@@ -0,0 +1,18 @@
+const express = require("express");
+var app = express();
+
+app.get("/blockchain", function(req, res){
+});
+
+app.post("/transaction", function(req, res){
+
+});
+
+app.get("/mine", function(req, res){
+
+})
+
+
+app.listen(3000, function(){
+    console.log("listening at 3000...");
+});
