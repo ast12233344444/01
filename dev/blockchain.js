a7c77203217f2229539d2a91b9072dbf284333eb
ast12233344444 <97659984+ast12233344444@users.noreply.github.com>
2022-01-20 13:20:49 +0300

    data str. completed

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,9 @@
+const sha256 = require("sha256");
+
 function Blockchain(){
     this.chain = [];
     this.newTransactions = [];
+    this.newBlock(0, "", "000000000000000000000000")
 }
 
 Blockchain.prototype.newBlock = function(nonce, lastBlockHash, hash){
@@ -16,6 +19,40 @@ Blockchain.prototype.newBlock = function(nonce, lastBlockHash, hash){
     this.chain.push(newBlock);
 
     return newBlock;
+}
+
+Blockchain.prototype.getLastBlock = function(){
+    return this.chain[this.chain.length - 1];
+}
+
+Blockchain.prototype.addNewTransaction = function(amount, sender, receiver){
+    const newTransaction = {
+        amount: amount,
+        sender:sender,
+        receiver:receiver
+    };
+
+    this.newTransactions.push(newTransaction);
+
+    return this.getLastBlock().index + 1;
+}
+
+Blockchain.prototype.hashBlock = function(previousHash, currentBlock, nonce){
+    const string = previousHash + nonce.toString() + JSON.stringify(currentBlock);
+    const hash = sha256(string);
+    return hash;
+}
+
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData){
+    let hash;
+    for(let nonce =0; true; nonce++){
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+        console.log(hash);
+        if(hash.substring(0,4) == "0000"){
+            return nonce;
+        }
+    }
+    
 }
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,8 +1,29 @@
 const Blockchain = require('./blockchain.js');
 
 const bitcoin = new Blockchain();
-bitcoin.newBlock(12425, "daewgsdhgwrfgrwsgwer", "hgsfaesfwsrg");
-bitcoin.newBlock(36745, "hdtjrföhjsrfgsrhr", "asegswrhwsrhnsrg");
-bitcoin.newBlock(78567, "ghdtjfrykfrhjdfhgdrh", "sdfghetsergsa");
 
-console.log(bitcoin);
+const previousBlockHash = "GWSRHHJD08T7DHD087HDGH";
+
+const currentBlockData = [
+    {
+        amount: 5,
+        sender: "SRHRJREDTYJIEDRSW",
+        receiver: "HJDGJGJDTHYKFHJDG"
+    },
+    {
+        amount: 10,
+        sender: "SRHRJHDTKMJDHRWRSW",
+        receiver: "HJDGJGTJEDTGHSRHJDG"
+    },
+    {
+        amount: 5,
+        sender: "SRHRDSFHFHSGSEDGSHW",
+        receiver: "HJDGSBSFHSRHDFMNGJNDG"
+    }
+]
+
+const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+
+const currentHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+console.log(nonce);
